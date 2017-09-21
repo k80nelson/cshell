a128a5fbe9f923de44733e9458c7e526b92205b4
Katie Nelson <contact@ketnelson.com>
2017-09-20 20:15:40 -0500

    add shell functionality

--- shell.c
+++ shell.c
@@ -1,4 +1,8 @@
 #include <stdio.h>
+#include <signal.h>
+#include <errno.h>
+#include <string.h>
+#include <unistd.h>
 #include <stdlib.h>
 #include <readline/readline.h>
 
@@ -35,18 +39,48 @@ static void handler(int signum){
 }
 
 int main(int argc, char** argv, char** envp){
-    char* user = getenv("USER");
-    strcat(user, " $ ");
+    char* prompt = getenv("USER");
+    strcat(prompt, " $ ");
 
     char* args[ARR_SIZE];
     size_t nargs;
     
+    pid_t pid;
+    int* ret_status;
+    
+    struct sigaction sa;
+    sa.sa_handler = &handler;
+    sa.sa_flags = SA_RESTART;
+    sigemptyset(&sa.sa_mask);
+    sigaction(SIGCHLD, &sa, NULL);
+
+    int foreground = 1;
+ 
     while(1){
-        char* line = readline(user);
+        char* line = readline(prompt);
 	parse_args(line, args, ARR_SIZE, &nargs);
+
         if (nargs>0) add_history(line);
 	else continue;
+
         if (!strcmp(args[0], "exit")) exit(0);
+
+	if (strcmp(args[nargs-1], "&")){
+	    foreground = 1;
+	} else{
+	    foreground = 0;
+            args[nargs-1] = NULL;
+        }
+
+        pid = fork();
+	if (!pid){
+	    if(execvpe(args[0], args, envp)){
+	        puts(strerror(errno));
+	        exit(127);
+	    }
+        }else if (foreground){
+	    pid = wait(ret_status);
+        }
     }
     return 0;
 }
