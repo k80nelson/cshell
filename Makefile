6c9834b4923c9668850262ba927d311077593553
Katie Nelson <contact@ketnelson.com>
2017-09-20 19:30:44 -0500

    Add exit functionality

--- Makefile
+++ Makefile
@@ -1,2 +1,2 @@
-shell: 
-	gcc -o shell shell.c -L/usr/local/lib -I/usr/local/include -lreadline
+shell: shell.c 
+	gcc -Wall -o shell.exe shell.c -L/usr/local/lib -I/usr/local/include -lreadline
--- shell.c
+++ shell.c
@@ -1,18 +1,20 @@
-#include <readline/readline.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <readline/readline.h>
 
 #define BUFFER_SIZE = 1<<16
 #define ARR_SIZE = 1<<16
 
-
 int main(int argc, char** argv, char** envp){
-    char user = get
+    char* user = getenv("USER");
+    strcat(user, " $ ");
     while(1){
-        
+        char* line = readline(user);
+        if (strlen(line)>0) add_history(line);
+	else continue;
 
+        if (!strcmp(line, "exit")) exit(0);
 
     }
-
     return 0;
 }
